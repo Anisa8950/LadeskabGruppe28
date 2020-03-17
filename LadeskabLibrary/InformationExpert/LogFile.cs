5c4f845bd4c80de60e860f7397c140fba20d5078
Stonani <stefine.varde@gmail.com>
2020-03-17 08:27:59 +0100

    Oprettelse af logfil og lille test i program af logifl funktion

--- LadeskabLibrary/InformationExpert/LogFile.cs
+++ LadeskabLibrary/InformationExpert/LogFile.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LadeskabLibrary
 {
     public class LogFile : ILogFile
     {
+
         public void LogDoorLocked(string id)
         {
-            throw new NotImplementedException();
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\stefi\\source\\repos\\LadeskabGruppe28", true))
+            {
+                try
+                {
+                    sw.WriteLine(DateTime.Now + ": Skab låst med RFID: " + id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
         }
 
         public void LogDoorUnlocked(string id)
         {
-            throw new NotImplementedException();
+            using (StreamWriter sw = new StreamWriter(@"..\\..\\..\\LogFil.txt", true))
+            {
+                try
+                {
+                    sw.WriteLine(DateTime.Now + ": Skab låst op med RFID: " + id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
         }
     }
 }
