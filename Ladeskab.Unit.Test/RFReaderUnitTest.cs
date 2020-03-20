4f567accda4c7c57e3342886c4d9b2139cf083c1
Marie-Louise <marie_louise_ryom@hotmail.com>
2020-03-20 17:08:19 +0100

    tilføjelse af unittest af iddetected event

---
+++ Ladeskab.Unit.Test/RFReaderUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+using LadeskabLibrary;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    public class RFReaderUnitTest
+    {
+        private RFReader _uut;
+        private RFDetectedEventArgs _receivedEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _receivedEventArgs = null;
+
+            _uut = new RFReader();
+            _uut.scan(123456);
+
+            _uut.IdDetectedEvent +=
+                (o, args) =>
+                {
+                    _receivedEventArgs = args;
+                };
+        }
+
+        [Test]
+        public void ScanId_IdSetToNewValue_EventFired()
+        {
+            _uut.scan(654321);
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+        }
+
+        [Test]
+        public void ScanId_IdSetToNewValue_CorrectNewIdReceived()
+        {
+            _uut.scan(654321);
+            Assert.That(_receivedEventArgs.IdDetected, Is.EqualTo(654321));
+        }
+    }
+}
--- Ladeskab.Unit.Test/StationControlUnitTest.cs
+++ Ladeskab.Unit.Test/StationControlUnitTest.cs
@@ -1,12 +1,27 @@
 using NUnit.Framework;
+using NSubstitute;
+using LadeskabLibrary;
 
 namespace Ladeskab.Unit.Test
 {
+    [TestFixture]
     public class StationControlUnitTest
     {
+        private StationControl _uut;
+        private IDisplay _display;
+        private IDoor _door;
+        private ILogFile _logFile;
+        private IRFReader _idSource;
+        
+
         [SetUp]
         public void Setup()
         {
+            _display = Substitute.For<IDisplay>();
+            _door = Substitute.For<IDoor>();
+            _logFile = Substitute.For<ILogFile>();
+            _idSource = Substitute.For<IRFReader>();
+            
         }
 
         [Test]
