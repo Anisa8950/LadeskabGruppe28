5c0aea421ffb79649ba1586056fe9eae07bf3b1c
Rahima Khouani <201806699@post.au.dk>
2020-03-20 20:32:59 +0100

    Added tests to usbcharger unittest

--- Ladeskab.Unit.Test/UsbChargerUnitTest.cs
+++ Ladeskab.Unit.Test/UsbChargerUnitTest.cs
@@ -21,12 +21,37 @@ namespace Ladeskab.Unit.Test
         }
 
         [Test]
-        public void StartingCharger_()
+        public void StartingChargerCalled_IsConnectedAndNotOverload_CurrentValueIs500()
+        {
+            _uc.StartCharging();
+
+            Assert.That(_uc.CurrentValue, Is.EqualTo(500));
+
+        }
+
+        [Test]
+        public void StartingChargerCaller_IsConnectedAndOverload_CurrentValueEqualsOverloadCurrent()
+        {
+            _uc.SimulateOverload(false);
+            _uc.StartCharging();
+
+            Assert.That(_uc.CurrentValue,Is.EqualTo(_uc.overload)); // der er ikke nogen property til overloadCurrent, hvordan tjekker man at currentvalue er lig med den. Siger man så 750
+            Assert.That(_uc.Connected,Is.EqualTo(true));
+        }
+
+        [Test]
+        public void StartingChargerCaller_IsNotConnected_CurrentValueEquals0()
         {
 
-            //
-            Assert.Pass();
+            _uc.Connected = false;
+            _uc.StartCharging();
+
+            Assert.That(_uc.CurrentValue,Is.EqualTo(0.0));
+
         }
+
+
+
 
 
 
