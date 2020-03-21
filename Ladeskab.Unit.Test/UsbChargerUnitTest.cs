966117b6d7810bcff2d7fa830bebf8a0928d50c6
Anisa Mohamed Nuur Ali <201806371@post.au.dk>
2020-03-21 08:08:29 +0100

    Rettet navnet for testklasse _uut istedet for _uc

--- Ladeskab.Unit.Test/UsbChargerUnitTest.cs
+++ Ladeskab.Unit.Test/UsbChargerUnitTest.cs
@@ -10,43 +10,43 @@ namespace Ladeskab.Unit.Test
     class UsbChargerUnitTest
     {
 
-        private UsbCharger _uc;
+        private UsbCharger _uut;
         
 
         [SetUp]
         public void Setup()
         {
 
-            _uc = new UsbCharger();
+            _uut = new UsbCharger();
         }
 
         [Test]
         public void StartingChargerCalled_IsConnectedAndNotOverload_CurrentValueIs500()
         {
-            _uc.StartCharging();
+            _uut.StartCharging();
 
-            Assert.That(_uc.CurrentValue, Is.EqualTo(500));
+            Assert.That(_uut.CurrentValue, Is.EqualTo(500));
 
         }
 
         [Test]
         public void StartingChargerCaller_IsConnectedAndOverload_CurrentValueEqualsOverloadCurrent()
         {
-            _uc.SimulateOverload(false);
-            _uc.StartCharging();
+            _uut.SimulateOverload(false);
+            _uut.StartCharging();
 
            // Assert.That(_uc.CurrentValue,Is.EqualTo(_uc.overload)); // der er ikke nogen property til overloadCurrent, hvordan tjekker man at currentvalue er lig med den. Siger man så 750
-            Assert.That(_uc.Connected,Is.EqualTo(true));
+            Assert.That(_uut.Connected,Is.EqualTo(true));
         }
 
         [Test]
         public void StartingChargerCaller_IsNotConnected_CurrentValueEquals0()
         {
 
-            _uc.Connected = false;
-            _uc.StartCharging();
+            _uut.Connected = false;
+            _uut.StartCharging();
 
-            Assert.That(_uc.CurrentValue,Is.EqualTo(0.0));
+            Assert.That(_uut.CurrentValue,Is.EqualTo(0.0));
 
         }
 
