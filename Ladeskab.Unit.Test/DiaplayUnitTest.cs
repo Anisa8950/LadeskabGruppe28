59e29db759436404576b386f1a1a89ecba3096c2
Marie-Louise <marie_louise_ryom@hotmail.com>
2020-03-21 12:53:22 +0100

    tilføjelse af unittest af display

---
+++ Ladeskab.Unit.Test/DiaplayUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using LadeskabLibrary;
+
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    public class DiaplayUnitTest
+    {
+        private Display _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+    }
+}
