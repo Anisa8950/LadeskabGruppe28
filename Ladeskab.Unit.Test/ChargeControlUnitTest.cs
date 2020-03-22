576c602a44a9f3ef560668131c7144a6345649ff
Anisa Mohamed Nuur Ali <201806371@post.au.dk>
2020-03-22 12:33:23 +0100

    Added final tets

--- Ladeskab.Unit.Test/ChargeControlUnitTest.cs
+++ Ladeskab.Unit.Test/ChargeControlUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using LadeskabLibrary;
 using NSubstitute;
 using NSubstitute.Core.Arguments;
@@ -7,26 +8,25 @@ using NSubstitute.ReceivedExtensions;
 
 namespace Ladeskab.Unit.Test
 {
-   [TestFixture]
+    [TestFixture]
     public class ChargeControlUnitTest
     {
         private ChargeControl _uut;
         private IDisplay _display;
         private IUsbCharger _usbCharger;
-        
+
 
 
         [SetUp]
         public void Setup()
         {
             // arrange
-        
             _display = Substitute.For<IDisplay>();
             _usbCharger = Substitute.For<IUsbCharger>();
-            _uut=new ChargeControl(_usbCharger, _display);
+            _uut = new ChargeControl(_usbCharger, _display);
         }
 
-
+        #region UUT is subcribed to event test
 
         [TestCase(5)]
         [TestCase(50)]
@@ -37,69 +37,91 @@ namespace Ladeskab.Unit.Test
             // Act: der bliver "raised an event"
             _usbCharger.CurrentLevelEvent += Raise.EventWith(new CurrentLevelEventArgs {Current = newCurrent});
 
-            // Assert: check for at currentvalue er blevet sat til den nye værdi efter: raise event
-            Assert.That(_uut.CurrentValue,Is.EqualTo(newCurrent));
-
-           
+            // Assert: Tjek for at currentvalue er blevet sat til den nye værdi efter: raise event
+            Assert.That(_uut.CurrentValue, Is.EqualTo(newCurrent));
         }
 
-        [Test]
-        public void StartChargerCalled_CurrentBelow500AndOver5_PrintChargingMobileAndStartChargingIsCalled()
+
+        #endregion
+
+        #region StartCharger()
+
+        [TestCase(50)]
+        [TestCase(100)]
+        [TestCase(200)]
+
+        public void StartCharger_CurrentBelow500AndOver5_PrintChargingMobileAndStartChargingIsCalled(double currentValue)
         {
-            //Flere testcases med currentvalue der får forskellige værdier
-           
-            _uut.CurrentValue = 50;
+            _uut.CurrentValue = currentValue;
             _uut.StartCharger();
 
             _usbCharger.Received(1).StartCharging();
             _display.Received(1).PrintChargingMobile();
-
+            _display.Received(0).PrintChargingError();
         }
 
-        [Test]
-        public void IsConnected_CurrentBelowZero_ReturnsFalse() // i tvivl om er rigtig
+        [TestCase(550)]
+        [TestCase(600)]
+        [TestCase(650)]
+
+        public void StartCharger_CurrentOver500_PrintChargingErrorIsCalled(double currentValue)
         {
-            _uut.CurrentValue = -3;
-            _uut.IsConnected();
+            _uut.CurrentValue = currentValue;
+            _uut.StartCharger();
 
-            Assert.That(_uut.IsConnected,Is.EqualTo(false));
-
+            _display.Received(1).PrintChargingError();
         }
 
-      
 
+        #endregion
+
+        #region IsConnected()
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-20)]
+        public void IsConnected_CurrentBelowZero_ReturnFalse(double currentValue) 
+        {
+            _uut.CurrentValue = currentValue;
+            _uut.IsConnected();
+            Assert.That(_uut.IsConnected,Is.EqualTo(false));
+        }
 
         [Test]
-        public void IsConnected_CurrentIsZero_ReturnsFalse() // i tvivl om er rigtig
+        public void IsConnected_CurrentIsZero_ReturnFalse()
         {
             _uut.CurrentValue = 0;
             _uut.IsConnected();
-
             Assert.That(_uut.IsConnected, Is.EqualTo(false));
-
         }
 
-        [Test]
-        public void IsConnected_CurrentOverZero_ReturnsTrue() // i tvivl om er rigtig
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void IsConnected_CurrentOverZero_ReturnTrue(double currentValue) 
         {
-            _uut.CurrentValue = 5;
+            _uut.CurrentValue = currentValue;
             _uut.IsConnected();
-
             Assert.That(_uut.IsConnected, Is.EqualTo(true));
-
         }
 
+        #endregion
 
+        #region StopCharger()
 
-        [Test]
-        public void StopCharger_CurrentBelow5Ampere_ChargerIsStoppendAndDisplayPrints()
+        [TestCase(1)]
+        [TestCase(2.5)]
+        [TestCase(3.5)]
+        public void StopCharger_CurrentBetween0to5_ChargerIsStoppendAndDisplayPrints(double currentValue)
         {
-            _uut.CurrentValue = 2.5;
+            _uut.CurrentValue = currentValue;
             _uut.StopCharger();
             _usbCharger.Received(1).StopCharging();
             _display.Received(1).PrintChargingComplete();
-
         }
+
+        #endregion
+
 
 
     }
--- Ladeskab.Unit.Test/UsbChargerUnitTest.cs
+++ Ladeskab.Unit.Test/UsbChargerUnitTest.cs
@@ -26,13 +26,13 @@ namespace Ladeskab.Unit.Test
         #region Initial State test
      
         [Test]
-        public void ctor_IsConnected()
+        public void InitialState_IsConnected()
         {
             Assert.That(_uut.Connected, Is.True);
         }
 
         [Test]
-        public void ctor_CurentValueIsZero()
+        public void InitialState_CurentValueIsZero()
         {
             Assert.That(_uut.CurrentValue, Is.Zero);
         }
@@ -40,7 +40,7 @@ namespace Ladeskab.Unit.Test
 
         #endregion
 
-        #region Simulation Methods
+        #region Simulate disconnected
 
         [Test]
         public void SimulateDisconnected_ReturnsDisconnected()
@@ -50,17 +50,24 @@ namespace Ladeskab.Unit.Test
         }
 
 
-        [Test]
-        public void SimulateOverloaded_ReturnsOverloaded()
-        {
-            _uut.SimulateOverload(false);
-            Assert.That(_uut.Connected, Is.False);
-        }
-
         #endregion
 
         #region StartCharging(), testing event
-        [Test] // kopiret fra Frank
+        
+        [Test]
+        public void startCharging_chargerStarted_EventFired()
+        {
+            _receivedEventArgs = null;
+
+            _uut.CurrentLevelEvent +=
+                (o, args) => { _receivedEventArgs = args; };
+
+            _uut.StartCharging();
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+        }
+        
+        
+        [Test] 
         public void Started_WaitSomeTime_ReceivedSeveralValues()
         {
             int numValues = 0;
@@ -73,22 +80,27 @@ namespace Ladeskab.Unit.Test
             Assert.That(numValues, Is.GreaterThan(4));
         }
 
-        [Test]
-        public void startCharging_chargerStarted_EventFired()
-        {
-            _receivedEventArgs = null;
-            _uut.CurrentLevelEvent +=
-                (o, args) => { _receivedEventArgs = args; };
-
-            _uut.StartCharging();
-            Assert.That(_receivedEventArgs, Is.Not.Null);
-        }
+    
 
 
         #endregion
 
         #region StartCharging(), testing for right values
+        
+        [Test]
+        public void StartCharging_IsCharging_Receives500AmpereImmediatly()
+        {
+            double CurrentLevel = 0;
 
+            _uut.CurrentLevelEvent +=
+                (o, args) => { CurrentLevel = args.Current; };
+
+            _uut.StartCharging();
+
+            Assert.That(CurrentLevel, Is.EqualTo(500));
+        }
+        
+        
         [Test]
         public void Started_WaitSomeTime_ReceivedChangedValue()
         {
@@ -126,25 +138,14 @@ namespace Ladeskab.Unit.Test
         }
 
 
-        [Test]
-        public void StartCharging_IsCharging_Receives500AmpereImmediatly()
-        {
-            double CurrentLevel = 0;
 
-            _uut.CurrentLevelEvent +=
-                (o, args) => { CurrentLevel = args.Current; };
-
-            _uut.StartCharging();
-
-            Assert.That(CurrentLevel, Is.EqualTo(500));
-        }
 
         #endregion
 
         #region StartCharging(), Overloaded
 
         [Test]
-        public void StartingChargerCaller_IsConnectedAndOverload_CurrentValueIsOverloadCurrent()
+        public void StartingCharging_IsConnectedAndOverload_CurrentValueIsOverloadCurrent()
         {
             _uut.SimulateOverload(false);
             _uut.StartCharging();
@@ -152,14 +153,7 @@ namespace Ladeskab.Unit.Test
             // Assert.That(_uc.CurrentValue,Is.EqualTo(_uc.overload)); // der er ikke nogen property til overloadCurrent, hvordan tjekker man at currentvalue er lig med den. Siger man så 750
             Assert.That(_uut.Connected, Is.EqualTo(true));
         }
-        [Test]
-        public void StartingChargerCalled_IsConnectedAndNotOverload_CurrentValueIs500()
-        {
-            _uut.StartCharging();
-
-            Assert.That(_uut.CurrentValue, Is.EqualTo(500));
-
-        }
+        //Svar: Man kan ikke teste direkte at den tilstanden er overloaded, men man kan teste for at værdien på strømmen er over 750 ampere
 
         [Test]
         public void SimulateOverload_Start_ReceivesHighValueImmediately()
@@ -261,16 +255,37 @@ namespace Ladeskab.Unit.Test
             Assert.That(lastValue, Is.Zero);
         }
 
-
         [Test]
-        public void StartingChargerCaller_IsNotConnected_CurrentValueIsZero()
+        public void SimulateDisconnected_Start_PropertyValueIsZeroImmediately()
         {
+            // First value should be high
+            _uut.SimulateConnected(false);
 
-            _uut.Connected = false;
+            // Start
             _uut.StartCharging();
 
-            Assert.That(_uut.CurrentValue, Is.EqualTo(0.0));
+            // Should not wait for first tick, should send zero immediately
 
+            Assert.That(_uut.CurrentValue, Is.Zero);
+        }
+        [Test]
+        public void Started_SimulateDisconnected_PropertyValueIsZero()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+
+            // Start
+            _uut.StartCharging();
+
+            // Next value should be zero
+            _uut.SimulateConnected(false);
+
+            // Reset event
+            pause.Reset();
+
+            // Wait for next tick, should send disconnected value
+            pause.WaitOne(300);
+
+            Assert.That(_uut.CurrentValue, Is.Zero);
         }
         #endregion
 
@@ -298,7 +313,7 @@ namespace Ladeskab.Unit.Test
 
             System.Threading.Thread.Sleep(300);
 
-            _uut.StartCharging();
+            _uut.StopCharging();
 
             Assert.That(lastValue, Is.EqualTo(0.0));
         }
@@ -307,7 +322,7 @@ namespace Ladeskab.Unit.Test
         [Test]
         public void StopCharge_IsCharging_ReceivesNoMoreValues()
         {
-            double lastValue = 1000;
+            double lastValue = 0;
             _uut.CurrentLevelEvent += (o, args) => lastValue = args.Current;
 
             _uut.StartCharging();
